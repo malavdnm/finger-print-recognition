f78ec3eecb9568c7bf5603804610826e6235ef57
malavdnm <38399975+malavdnm@users.noreply.github.com>
2020-08-24 22:36:55 +0530

    Add files via upload

---
+++ BLPOC.py
@@ -0,0 +1,72 @@
+import numpy as np
+import cv2 
+from PIL import Image
+
+def BLPOC(input_image, reg_image_corrected, k1, k2):
+	#rows, cols = reg_image_corrected.shape
+	#k1 = int(rows*0.7)
+	#k2 = int(cols*0.7)
+	#l1 = 2*k1 + 1
+	#l2 = 2*k2 + 1
+
+	# print input_image.shape
+	# print reg_image_corrected.shape
+	fft_input = np.fft.fft2(input_image)
+	fft_shift_input = np.fft.fftshift(fft_input)
+	fft_reg = np.fft.fft2(reg_image_corrected)
+	fft_shift_reg = np.fft.fftshift(fft_reg)
+	#rows, cols = fft_input.shape
+	# print fft_shift_input.shape
+
+	fft_input_conj = np.matrix.conjugate(fft_shift_input)
+	# print fft_shift_reg.shape
+	# print fft_input_conj.shape
+	cross_phase_spec = np.divide(np.multiply(fft_shift_reg, fft_input_conj), (np.absolute(np.multiply(fft_shift_reg, fft_input_conj))))
+	rows, cols = cross_phase_spec.shape
+	k1 = rows//6
+	k2 = cols//6
+	m1 = rows//2
+	m2 = cols//2
+	#diff_row = m1 - k1
+	#diff_col = m2 - k2
+	#left_extreme = m2 - k2 
+	#right_extreme = 2*m2 - (diff_row) 
+	#top_extreme = m1 - k1 
+	#bottom_extreme = 2*m1 - (diff_col)
+	#eff_rows = (bottom_extreme - top_extreme) + 1
+	#eff_cols = (right_extreme - left_extreme) + 1
+	#blpoc_fft = np.zeros((eff_rows, eff_cols), dtype = complex)
+	blpoc_fft = cross_phase_spec[m1-k1:m1+k1,m2-k2:m2+k2]
+
+	#blpoc_fft = cross_phase_spec[top_extreme:bottom_extreme, left_extreme:right_extreme]
+
+	blpoc_fft_shifted = np.fft.ifftshift(blpoc_fft)
+	blpoc_ifft = np.fft.ifft2(blpoc_fft_shifted)
+	blpoc_ifft = np.absolute(blpoc_ifft)
+	# blpoc_img = Image.fromarray(blpoc_ifft*255)
+	# print blpoc_ifft
+	# blpoc_img.show()
+	# print blpoc_ifft
+	blpoc_peak_value = np.amax(blpoc_ifft)
+	blpoc_ifft[np.where(blpoc_ifft==np.max(blpoc_ifft))] = -10000
+	second_lrg = np.amax(blpoc_ifft)
+	#second_lrg = np.partition(blpoc_ifft.flatten(), -2)[-2]
+	blpoc_peak_value = (blpoc_peak_value + second_lrg)/2
+
+	return blpoc_peak_value
+
+if __name__ == '__main__' :
+	input_image = cv2.imread('Input Image.tif', 0)
+	reg_image_corrected = cv2.imread('Registered Image.tif', 0)
+	a = BLPOC(input_image, input_image, 150, 150)
+	print(a)
+
+
+			
+
+
+		
+		
+
+
+
---
+++ alignment_cores.py
@@ -0,0 +1,32 @@
+import numpy as np 
+import cv2 
+from PIL import Image
+
+def align_cores(input_core_coord, reg_img_coord, input_img_path, reg_img_path):
+
+	input_img = cv2.imread(input_img_path, 0)
+	reg_img = cv2.imread(reg_img_path, 0)
+	# reg_img_arr = np.asarray(reg_img)
+	# input_arr = np.asarray(input_img)
+	print("lala")
+	print(input_core_coord)
+	print(reg_img_coord)
+	disp_x =  input_core_coord[0] - reg_img_coord[0] 
+	disp_y = input_core_coord[1] - reg_img_coord[1] 
+	rows, cols = reg_img.shape
+	transformation = np.float32([[1, 0, disp_y], [0, 1, disp_x]])
+	displaced_reg = np.zeros((rows, cols))
+	displaced_reg = cv2.warpAffine(reg_img, transformation, (cols, rows))
+	# print displaced_reg.shape
+	img = Image.fromarray(displaced_reg)
+	# img.show()
+	print("alignment cores done")
+	return displaced_reg
+	
+
+
+
+
+
+			
+	 	
